1fae05d201184d1dcbab67829532279fd2fcc5c5
Elena <102152365+ev939393@users.noreply.github.com>
2023-01-08 15:40:47 +0300

    feat: task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,10 @@
 module.exports = function reverse (n) {
-  
+    let arr = String(Math.abs(n)).split('');
+    let result = '';
+
+    while(arr.length > 0) {
+      result += arr.pop();
+    }
+    
+    return Number(result);
 }
